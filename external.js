ca96c7023298a133d9dd6e7650f22957c7231168
bakos <=abebeb>
2021-09-23 10:19:37 +0300

    result added

--- external.js
+++ external.js
@@ -1,3 +1,6 @@
+let commputerScore = 0;
+let humanScore = 0
+let round = 0;
 function computerPlay() {
     let num = Math.floor(Math.random() * 3) + 1;
     let result = ['Rock', 'Paper', 'Scissors'];
@@ -6,19 +9,34 @@ function computerPlay() {
 }
 function playRound(playerSelection, computerSelection) {
     if (playerSelection.toLowerCase() != computerSelection.toLowerCase()) {
-        return "You Lose! Computer beats " + computerSelection
+        return 0;
     } else {
-        return "You Win! Congratulation"
+        return 1;
     }
 }
 function game(playerSelection) {
-    let computerResult = computerPlay();
-    //let playerSelection = prompt("Please Enter Your Weapon!");
-    let result = playRound(playerSelection, computerResult);
-    console.log(result)
-    document.getElementById('resultId').innerHTML = result;
-    document.getElementById('commputerSelectionId').innerHTML = "Commputer:" + computerResult;
-    document.getElementById('manSelectionId').innerHTML = "Human:" + playerSelection;
-
-    // return result;
+    if (round <= 15) {
+        let computerResult = computerPlay();
+        let message = '';
+        //let playerSelection = prompt("Please Enter Your Weapon!");
+        let result = playRound(playerSelection, computerResult);
+        if (result === 0) {
+            message = "You Lose! Computer beats " + computerResult;
+            commputerScore += 1;
+        }
+        if (result === 1) {
+            message = "You Win! Congratulation";
+            humanScore += 1;
+        }
+        round += 1;
+        document.getElementById('resultId').innerHTML = message;
+        document.getElementById('commputerSelectionId').innerHTML = "Commputer:" + computerResult;
+        document.getElementById('manSelectionId').innerHTML = "Human:" + playerSelection;
+        document.getElementById('humanScoreId').innerHTML = "Human Score:" + commputerScore;
+        document.getElementById('commputerScoreId').innerHTML = "Commputer Score:" + humanScore;
+        console.log(message)
+    }
+    else {
+        document.getElementById('gameOverId').innerHTML = "Game Over try again";
+    }
 }
